ee64b7efe7f12b45a86cec1cf7a15f8c86c33433
olhasemenova <olha.sergeevna87@gmail.com>
2022-10-25 11:24:47 -0400

    corrected conditions in task 2; code refactoring in task3

--- HW3/if_loop.py
+++ HW3/if_loop.py
@@ -1,25 +1,26 @@
 # Пользователь вводит с клавиатуры три числа в переменные a, b, c. Если все числа больше 10 и первые два числа делятся на 3, то вывести yes, иначе no
 
-a = int(input('Enter number a: '))
-b = int(input('Enter number b: '))
-c = int(input('Enter number c: '))
+# a = int(input('Enter number a: '))
+# b = int(input('Enter number b: '))
+# c = int(input('Enter number c: '))
+#
+# if a > 10 and a % 3 == 0 and b > 10 and b % 3 == 0 and c > 10:
+#     print('yes')
+# else:
+#     print('no')
 
-if a > 10 and a % 3 == 0 and b > 10 and b % 3 == 0 and c > 10:
-    print('yes')
-else:
-    print('no')
-
-
+# Задание #2 (-5 баллов):Если все числа будут равны, то код выдаст ошибку
 # Пользователь вводит с клавиатуры три числа в переменные a, b, c. Найдите наибольшее число из них и запишите в переменную max.
 
 a = int(input('Enter number a: '))
 b = int(input('Enter number b: '))
 c = int(input('Enter number c: '))
-if a > b and a > c:
+
+if (a > b and a > c) or (a > b == c) or (a == b == c):
     max = a
-elif b > a and b > c:
+elif (b > a and b > c) or (b > a == c):
     max = b
-elif c > a and c > b:
+elif (c > a and c > b) or (c > a == b):
     max = c
 print(max)
 
@@ -28,15 +29,13 @@ print(max)
 A = int(input('Enter number A: '))
 B = int(input('Enter number B: '))
 total = 0
-if A > B:
-    for i in range(B,A+1):
-        if i % 2 == 0:
-            total += i
-    print(total)
-elif A < B:
-    for i in range(A,B+1):
-        if i % 2 == 0:
-            total += i
-    print(total)
+
+if A == B:
+    print('A and B should have different values')
 else:
-    print('A and B should have different values!')
+    if A > B:
+        A, B = B, A
+    for i in range(A, B + 1):
+        if i % 2 == 0:
+            total += i
+    print(total)
