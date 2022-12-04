1f60a86c36c48ca5743abf9dfe173b39f3f1bd87
olhasemenova <olha.sergeevna87@gmail.com>
2022-12-04 11:47:45 -0500

    change map func realization

--- HW 12/map.py
+++ HW 12/map.py
@@ -7,10 +7,16 @@
 from typing import Callable, Iterable
 
 def sum(*args):
-    return [x + y for x, y in zip(*args)]
-
+    result = []
+    for i in zip(*args):
+        sum = 0
+        for j in i:
+            sum += j
+        result.append(sum)
+    return result
 def custom_map(function: Callable, *args: Iterable) -> Iterable:
     result = function(*args)
     return result
 
 print(custom_map(sum, [1, 2, 3], [3, 5, 0, 5]))
+print(custom_map(sum, [1, 2, 3], [3, 5, 0, 5], [1, 1, 1]))
