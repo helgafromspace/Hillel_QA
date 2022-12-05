ba65feb41fcc9098f8a7c2d51a5e7964c96dffb0
olhasemenova <olha.sergeevna87@gmail.com>
2022-12-05 14:24:25 -0500

    change zip func realization

--- HW13/zip.py
+++ HW13/zip.py
@@ -12,26 +12,42 @@
 
 seq1 = [1, 2, 3, 4, 5]
 seq2 = [9, 8, 7]
-
+seq3 = [5, 5, 2, 1]
 
 from typing import Iterable, List, Tuple
 
-def custom_zip(*sequences: Iterable, full=False, default=None) -> List[Tuple]:
-    result = []
-    long_seq = max(sequences, key=len)
-    short_seq = min(sequences, key=len)
-    if full:
-        for i in range(len(long_seq)):
-            if i < len(short_seq):
-                result.append((long_seq[i], short_seq[i]))
-            else:
-                result.append((long_seq[i], default))
-    else:
-        for i in range(len(short_seq)):
-            result.append((long_seq[i], short_seq[i]))
-    return result
+# на 2 последовательности
 
-print(custom_zip(seq1, seq2))
+# def custom_zip(*sequences: Iterable, full=False, default=None) -> List[Tuple]:
+#     result = []
+#     long_seq = max(sequences, key=len)
+#     short_seq = min(sequences, key=len)
+#     if full:
+#         for i in range(len(long_seq)):
+#             if i < len(short_seq):
+#                 result.append((long_seq[i], short_seq[i]))
+#             else:
+#                 result.append((long_seq[i], default))
+#     else:
+#         for i in range(len(short_seq)):
+#             result.append((long_seq[i], short_seq[i]))
+#     return result
+
+# больше чем на 2
+def custom_zip(*args: Iterable, full=False, default=None) -> List[Tuple] :
+    min_seq = min (args, key=len)
+    result = [[x] for x in args[0]]
+    for i in range (1, len (args)) :
+        for j in range (len(result)):
+            if j < len(args[i]):
+                result[j].append(args[i][j])
+            else:
+                result[j].append(default)
+    if full:
+        return [tuple(i) for i in result]
+    else:
+        return [tuple(i) for i in result[:len(min_seq)]]
+print(custom_zip(seq1, seq2, seq3))
 print(custom_zip(seq1, seq2, full=False))
-print(custom_zip(seq1, seq2, full=True, default="Q"))
+print(custom_zip(seq1, seq2,seq3, full=True, default="Q"))
 print(custom_zip(seq1, seq2, full=True))
