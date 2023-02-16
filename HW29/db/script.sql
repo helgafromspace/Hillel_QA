8e6882bb7aa06c641d2f1090d2ad559c84705149
olhasemenova <olha.sergeevna87@gmail.com>
2023-02-16 15:13:02 -0500

    added tables with data and script

---
+++ HW29/db/script.sql
@@ -0,0 +1,13 @@
+
+--1. Установить SQLite
+
+--2. Создать таблицу Persons(name: string, favorite_color: string, profit: float)
+
+--3. Создать таблицу Cars(model: string, color: string, price: float)
+
+--4. Заполнить таблицы данными
+
+--5. Вывести для каждого человека, который может купить автомобиль,самый дешевый автомобиль его любимого цвета,
+-- который он может себе позволить (цена автомобиля должна быть меньше или равна дохода человека).Отсортировать по имени чловека в алфавитном порядке.
+
+select Persons.name, Cars.model,Cars.color,Cars.price, Persons.profit from Persons JOIN Cars ON Persons.favourite_color = Cars.color where price <= profit GROUP BY name HAVING MIN(price) ORDER BY name ASC;
