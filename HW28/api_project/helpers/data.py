52372732690e1fed1969638c541694d5b0b4d1be
olhasemenova <olha.sergeevna87@gmail.com>
2023-02-16 09:44:06 -0500

    single file for data to import into tests; rewrite create post as single method to include into test

---
+++ HW28/api_project/helpers/data.py
@@ -0,0 +1,20 @@
+class Data:
+    pass
+
+class CreatePostData(Data):
+    valid_data_1_post = {
+        'title': 'First post',
+        'body': 'Hello world!',
+    }
+
+    empty_data = {
+        'title': '',
+        'body': '',
+    }
+
+    valid_data_2_post = {
+        'title': 'Second post',
+        'body': 'Hello world 2!',
+    }
+
+
--- HW28/api_project/test_api_tests.py
+++ HW28/api_project/test_api_tests.py
@@ -10,6 +10,7 @@
 
 import requests
 
+from .helpers.data import CreatePostData
 from .helpers.headers import CreateUserHeaders
 from .helpers.helper_config import get_create_user_url
 from .helpers.status_codes import StatusCodes
@@ -18,12 +19,7 @@ from .helpers.helper_functions import create_post
 def test_create_post(create_user):
     req_user_id = create_user.json()["id"]
     url = get_create_user_url()
-    data = {
-        'title': 'First post',
-        'body': 'Hello world!'
-    }
-
-    created_post = create_post(req_user_id, data)
+    created_post = create_post(req_user_id, CreatePostData.valid_data_1_post)
     assert created_post.status_code == StatusCodes.CREATED.value
 
     res_user_id = created_post.json()['user_id']
@@ -33,18 +29,14 @@ def test_create_post(create_user):
     get_user_posts = requests.get(f'{url}/{req_user_id}/posts', headers=CreateUserHeaders.headers)
 
     assert res_user_id == req_user_id
-    assert res_user_title == data['title']
-    assert res_user_body == data['body']
+    assert res_user_title == CreatePostData.valid_data_1_post['title']
+    assert res_user_body == CreatePostData.valid_data_1_post['body']
     assert len(get_user_posts.json()) == 1
 
 def test_create_post_with_empty_data(create_user):
     req_user_id = create_user.json()["id"]
     url = get_create_user_url()
-    data = {
-        'title': '',
-        'body': ''
-    }
-    created_post = create_post(req_user_id, data)
+    created_post = create_post(req_user_id, CreatePostData.empty_data)
 
     assert created_post.status_code == StatusCodes.UNPROCESSABLE_ENTITY.value
     assert len(created_post.json()) == 2
@@ -61,19 +53,12 @@ def test_create_post_with_empty_data(create_user):
 def test_create_2_posts(create_user):
     req_user_id = create_user.json()["id"]
     url = get_create_user_url()
-    data = {
-        'title': 'First post',
-        'body': 'Hello world!'
-    }
-    created_1_post = create_post(req_user_id, data)
+
+    created_1_post = create_post(req_user_id, CreatePostData.valid_data_1_post)
 
     res_1_user_id = created_1_post.json()['user_id']
 
-    data2 = {
-        'title': 'Second post',
-        'body': 'Hello world 2!'
-    }
-    created_2_post = create_post(req_user_id, data2)
+    created_2_post = create_post(req_user_id, CreatePostData.valid_data_2_post)
 
     res_2_user_id = created_2_post.json()['user_id']
 
@@ -87,10 +72,10 @@ def test_create_2_posts(create_user):
     assert res_1_user_id == res_2_user_id
     assert posts_list[0]['user_id'] == req_user_id
     assert posts_list[1]['user_id'] == req_user_id
-    assert posts_list[0]['title'] == data2['title']
-    assert posts_list[0]['body'] == data2['body']
-    assert posts_list[1]['title'] == data['title']
-    assert posts_list[1]['body'] == data['body']
+    assert posts_list[0]['title'] == CreatePostData.valid_data_2_post['title']
+    assert posts_list[0]['body'] == CreatePostData.valid_data_2_post['body']
+    assert posts_list[1]['title'] == CreatePostData.valid_data_1_post['title']
+    assert posts_list[1]['body'] == CreatePostData.valid_data_1_post['body']
 
 
 # def test_get_info_after_del(create_user):
