3439cfa54bd37b41f533b6430a160619039b201e
olhasemenova <olha.sergeevna87@gmail.com>
2023-02-21 11:43:35 -0500

    add another solution script for 6 task

--- HW29/db/script.sql
+++ HW29/db/script.sql
@@ -19,4 +19,8 @@ select Persons.name, Cars.model,Cars.color,Cars.price, Persons.profit from Perso
 
 select Persons.name, IIF( Cars.price > profit,NULL,Cars.model) AS model,Cars.color,IIF(Cars.price > profit,NULL,Cars.price) AS price, Persons.profit from Persons JOIN Cars ON Persons.favourite_color = Cars.color GROUP BY name HAVING MIN(price) ORDER BY name ASC;
 
+select Persons.name, Cars.model,Cars.color,Cars.price, Persons.profit from Persons JOIN Cars ON Persons.favourite_color = Cars.color where price <= profit GROUP BY name HAVING MIN(price) UNION select Persons.name, NULL as model,Cars.color,NULL AS price, Persons.profit from Persons JOIN Cars ON Persons.favourite_color = Cars.color GROUP BY name HAVING MIN(price) and price > profit ORDER BY name ASC;
+
 select Persons2.name, IIF( Cars2.price > profit,NULL,Cars2.model) AS model,Cars2.color,IIF(Cars2.price > profit,NULL,Cars2.price) AS price, Persons2.profit from Persons2 JOIN Cars2 ON Persons2.favourite_color = Cars2.color GROUP BY name HAVING MIN(price) ORDER BY name ASC;
+
+select Persons2.name, Cars2.model,Cars2.color,Cars2.price, Persons2.profit from Persons2 JOIN Cars2 ON Persons2.favourite_color = Cars2.color where price <= profit GROUP BY name HAVING MIN(price) UNION select Persons2.name, NULL as model,Cars2.color,NULL AS price, Persons2.profit from Persons2 JOIN Cars2 ON Persons2.favourite_color = Cars2.color GROUP BY name HAVING MIN(price) and price > profit ORDER BY name ASC;
