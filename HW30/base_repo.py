1eaa8664c627e48a96d3e89f472a493aa8186aa7
olhasemenova <olha.sergeevna87@gmail.com>
2023-02-21 19:50:20 -0500

    add method to BookRepository class

--- HW30/base_repo.py
+++ HW30/base_repo.py
@@ -5,7 +5,6 @@ from helper_config import get_db_path
 
 class BaseRepository:
 
-    def __init__(self,db_path):
-        path = get_db_path()
+    def __init__(self, path = get_db_path()):
         self._connection = sqlite3.connect(f'{path}book.db', isolation_level=None)
         self._cursor = self._connection.cursor()
--- HW30/book_repo.py
+++ HW30/book_repo.py
@@ -6,26 +6,37 @@ from helper_config import get_db_path
 
 class Book:
 
-    def __init__(self, id, title, author, publish_year):
-        self.id = id
+    def __init__(self, book_id, title, author:str, publish_year):
+        self.book_id = book_id
         self.title = title
         self.author = author,
         self.publish_year = publish_year
 
+    def __repr__(self):
+        return f'Book: {self.book_id}, {self.title},{self.author},{self.publish_year}'
+
 class BookRepository(BaseRepository):
 
     def __init__(self):
-        path = get_db_path ()
-        super().__init__(f'{path}book.db')
-        self._cursor.execute('CREATE TABLE IF NOT EXISTS Book(book_id INTEGER PRIMARY KEY, title TEXT NOT NULL, author TEXT NOT NULL, publish_year INTEGER);')
+        super().__init__()
+        self._cursor.execute('CREATE TABLE IF NOT EXISTS Book(book_id INTEGER PRIMARY KEY, title TEXT, author TEXT, publish_year INTEGER);')
 
     def get_all_books(self):
         self._cursor.execute('select * from Book;')
         data = self._cursor.fetchall()
         return data
 
-book1= BookRepository()
-print(book1.get_all_books())
+    def add_book(self, book:Book):
+        self._cursor.execute ("INSERT INTO Book VALUES(?,?,?,?);", (book.book_id,book.title,book.author[0],book.publish_year))
 
+book_repository = BookRepository()
+print(book_repository.get_all_books())
+
+book1 = Book(1,'1984','George Orwell', 1949)
+print(book1)
+book_repository.add_book(book1)
+print(book_repository.get_all_books())
+# print(book_repository.get_all_books())
+# print(book_repository.get_all_books())
 
 
