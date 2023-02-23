bbe07bf7343988f71311411a94c045a88d333ac9
olhasemenova <olha.sergeevna87@gmail.com>
2023-02-23 08:49:56 -0500

    add methods ro2object to display results as objects

--- HW30/book_repo.py
+++ HW30/book_repo.py
@@ -6,29 +6,27 @@ from base_repo import BaseRepository
 
 class Book:
 
-    def __init__(self, book_id, title, author:str, publish_year):
-        self.book_id = book_id
+    def __init__(self, title, author, publish_year):
         self.title = title
         self.author = author
         self.publish_year = publish_year
-
     def __repr__(self):
-        return f'Book: {self.book_id}, {self.title},{self.author},{self.publish_year}'
+        return f'{self.title}, {self.author},{self.publish_year}'
 
 class BookRepository(BaseRepository):
 
     def __init__(self):
         super().__init__()
-        self._cursor.execute('CREATE TABLE IF NOT EXISTS Book(book_id INTEGER UNIQUE PRIMARY KEY, title TEXT, author TEXT, publish_year INTEGER);')
+        self._cursor.execute('CREATE TABLE IF NOT EXISTS Book(book_id INTEGER PRIMARY KEY, title TEXT, author TEXT, publish_year INTEGER);')
 
     def get_all_books(self):
-        self._cursor.execute('select * from Book;')
+        self._cursor.execute('select title,author,publish_year from Book;')
         data = self._cursor.fetchall()
-        return data
+        return [self.row2object(row) for row in data]
 
-    def add_book(self, *books):
+    def add_books(self, *books):
         for book in books:
-            self._cursor.execute ("INSERT INTO Book VALUES(?,?,?,?);", (book.book_id,book.title,book.author,book.publish_year))
+            self._cursor.execute ("INSERT INTO Book(title,author,publish_year) VALUES(?,?,?);", (book.title,book.author,book.publish_year))
 
     def update_publish_year(self,new_year:int,old_year:int, title):
         self._cursor.execute ("UPDATE Book SET publish_year = ? where publish_year = ? and title = ?;", (new_year,old_year,title))
@@ -36,15 +34,18 @@ class BookRepository(BaseRepository):
     def select_all_books_of_specific_publish_year(self,year:int):
         self._cursor.execute ("select title,author,publish_year from Book where publish_year = ?;",[year])
         data = self._cursor.fetchall()
-        return data
+        return [self.row2object(row) for row in data]
 
     def select_all_books_of_specific_author(self,author):
         self._cursor.execute ("select title,author,publish_year from Book where author = ?;",[author])
         data = self._cursor.fetchall()
-        return data
+        return [self.row2object(row) for row in data]
 
     def delete_all_books_of_specific_author(self,author):
         self._cursor.execute ("delete from Book where author = ?;", [author])
+
+    def row2object(self,row):
+        return Book(row[0],row[1],row[2])
 
 
 
@@ -52,17 +53,21 @@ book_repository = BookRepository()
 print(book_repository.get_all_books())
 
 
-book1 = Book(1,'1984','George Orwell',1949)
-book2 = Book(2,'The Laws','Plato',1979)
-book3 = Book(3,'A Brief History Of Time','Steven Hawking',2020)
-book4 = Book(4,'The Republic','Plato',2020)
-book5 = Book(5,'Animal Farm','George Orwell',2020)
+book1 = Book('1984','George Orwell',1949)
+book_repository.get_all_books()
 
-book_repository.add_book(book1)
-print(book_repository.get_all_books())
-book_repository.add_book(book2,book3, book4, book5)
+book2 = Book('The Laws','Plato',1979)
+book3 = Book('A Brief History Of Time','Steven Hawking',2020)
+book4 = Book('The Republic','Plato',2020)
+book5 = Book('Animal Farm','George Orwell',2020)
+#
+book_repository.add_books(book1)
+
 print(book_repository.get_all_books())
 
+book_repository.add_books(book2,book3, book4, book5)
+print(book_repository.get_all_books())
+#
 print(book_repository.select_all_books_of_specific_publish_year(2020))
 print(book_repository.select_all_books_of_specific_author('George Orwell'))
 book_repository.update_publish_year(1950,1949,'1984')
