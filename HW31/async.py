5e629779b935b1f8e37ce0ea563adf038745dd19
olhasemenova <olha.sergeevna87@gmail.com>
2023-03-03 09:56:50 -0500

    add 3 task

--- HW31/async.py
+++ HW31/async.py
@@ -13,19 +13,21 @@ import string
 import os
 import threading
 import math
+import time
 
-def file_generator(directory,number_of_files,size):
-    parent_dir = os.path.split(__file__)[0]
-    path = os.path.join (parent_dir, directory)
-    os.mkdir(path)
-    seq = string.digits + string.punctuation + string.ascii_letters
-    for i in range(1,number_of_files+1):
-        f = open(f'{directory}/file{i}.py','a')
-        rand_num = randint(size/2,size)
-        for _ in range(rand_num):
-            f.write(f'{seq[randint(0,len(seq)-1)]}')
 
-file_generator('files',10,20)
+# def file_generator(directory,number_of_files,size):
+#     parent_dir = os.path.split(__file__)[0]
+#     path = os.path.join (parent_dir, directory)
+#     os.mkdir(path)
+#     seq = string.digits + string.punctuation + string.ascii_letters
+#     for i in range(1,number_of_files+1):
+#         f = open(f'{directory}/file{i}.py','a')
+#         rand_num = randint(size/2,size)
+#         for _ in range(rand_num):
+#             f.write(f'{seq[randint(0,len(seq)-1)]}')
+#
+# file_generator('files',10,20)
 
 
 #ASCII solution
@@ -48,27 +50,32 @@ file_generator('files',10,20)
 def letter_counter_in_one_thread(directory, letter_to_find)
 """
 #
-def letter_counter_in_one_thread(directory, letter_to_find=0):
+def letter_counter_in_one_thread(directory, letter_to_find):
     files_list = os.listdir(directory)
+    result = 0
     for i in files_list:
         f = open(f'{directory}/{i}', 'r')
         text = f.read()
-        letter_to_find += len(text)
-    return letter_to_find
+        if letter_to_find in text:
+            result += len(text)
+    return result
 
-print(f"letter_counter_in_one_thread result: {letter_counter_in_one_thread('files')}")
+print(f"letter_counter_in_one_thread result: {letter_counter_in_one_thread('files','g')}")
 
-# def letter_counter_in_one_thread(directory, letter_to_find={}):
+
+
+# def letter_counter_in_one_thread(directory, letter_to_find, output={}):
 #     files_list = sorted(os.listdir(directory))
 #     result = 0
 #     for i in files_list:
 #         f = open(f'{directory}/{i}', 'r')
 #         text = f.read()
-#         result+= len(text)
-#     letter_to_find['value'] = result
-#     return letter_to_find['value']
+#         if letter_to_find in text:
+#             result += len(text)
+#     output['value'] = result
+#     return output['value']
 #
-# print(f"letter_counter_in_one_thread result: {letter_counter_in_one_thread('files')}")
+# print(f"letter_counter_in_one_thread result: {letter_counter_in_one_thread('files','g')}")
 
 """
 3. Написать функцию, которая возвращает число букв `letter_to_find` во всех файлах директории `directory`
@@ -80,16 +87,18 @@ print(f"letter_counter_in_one_thread result: {letter_counter_in_one_thread('file
 def letter_counter_in_n_threads(directory, letter_to_find, number_of_threads)
 
 """
-def letter_counter_in_one_thread_for_group(directory,group, letter_to_find={}):
+def letter_counter_in_thread_for_group(directory,group, letter_to_find, output ={}):
     result = 0
     for file in group:
         f = open(f'{directory}/{file}', 'r')
         text = f.read()
-        result+= len(text)
-    letter_to_find['value'] = result
-    return letter_to_find['value']
+        if letter_to_find in text:
+            result += len(text)
+    output['value'] = result
+    return output['value']
 
-def letter_counter_in_n_threads(directory, number_of_threads, letter_to_find=0):
+#
+def letter_counter_in_n_threads(directory, number_of_threads, letter_to_find, output=0):
     files_list = os.listdir(directory)
     num_of_files = math.ceil(int(len(files_list)/number_of_threads))
     groups = []
@@ -97,15 +106,15 @@ def letter_counter_in_n_threads(directory, number_of_threads, letter_to_find=0):
         groups.append(files_list[:num_of_files])
         files_list = files_list[num_of_files:]
     thread = [None] * number_of_threads
-    letter_to_find_one = {}
+    output_one = {}
     for i,group in enumerate(groups):
-        thread[i]=threading.Thread(target=letter_counter_in_one_thread_for_group,args = (directory,group,letter_to_find_one))
+        thread[i]=threading.Thread(target=letter_counter_in_thread_for_group,args = (directory,group,letter_to_find,output_one))
         thread[i].start()
         thread[i].join ()
-        letter_to_find += letter_to_find_one['value']
-    return letter_to_find
+        output += output_one['value']
+    return output
 
-print(f"letter_counter_in_n_threads result: {letter_counter_in_n_threads('files',5)}")
+print(f"letter_counter_in_n_threads result: {letter_counter_in_n_threads('files',5,'g')}")
 
 
 
